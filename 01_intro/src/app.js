284958e91eb0838066d6396da26569fe61d34c2f
SOunit <workbookwork7@gmail.com>
2022-04-19 10:03:14 -0700

    refactor class

--- 01_intro/src/app.js
+++ 01_intro/src/app.js
@@ -1,26 +1,10 @@
 const { json } = require('express');
 const express = require('express');
-const User = require('./user/User');
-const bcrypt = require('bcrypt');
+const UserRouter = require('./user/UserRouter');
 
 const app = express();
 
 app.use(json());
-
-app.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    // const user = {
-    //   username: req.body.username,
-    //   email: req.body.email,
-    //   password: hash,
-    // };
-    // const user = Object.assign({}, req.body, { password: hash });
-    const user = { ...req.body, password: hash };
-
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
-});
+app.use(UserRouter);
 
 module.exports = app;
