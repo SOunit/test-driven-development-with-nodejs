48b758cfad3fd77009d7264e4dd9f8bba48bcec6
SOunit <workbookwork7@gmail.com>
2022-04-19 11:09:25 -0700

    add username validation

--- 04_validator/__test__/UserRegister.spec.js
+++ 04_validator/__test__/UserRegister.spec.js
@@ -11,30 +11,32 @@ beforeEach(() => {
   return User.destroy({ truncate: true });
 });
 
-describe('User Registration', () => {
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P4ssword',
-    });
-  };
+const validUser = {
+  username: 'user1',
+  email: 'user1@mail.com',
+  password: 'P4ssword',
+};
 
+const postUser = (user = validUser) => {
+  return request(app).post('/api/1.0/users').send(user);
+};
+
+describe('User Registration', () => {
   it('returns 200 OK when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser(validUser);
     expect(response.statusCode).toBe(200);
     // .expect(200, done);
   });
 
   it('returns success message when signup request is valid', (done) => {
-    postValidUser().then((response) => {
+    postUser().then((response) => {
       expect(response.body.message).toBe('User created');
       done();
     });
   });
 
   it('saves the user to database', (done) => {
-    postValidUser().then(() => {
+    postUser().then(() => {
       // query user table
       User.findAll().then((userList) => {
         expect(userList.length).toBe(1);
@@ -44,7 +46,7 @@ describe('User Registration', () => {
   });
 
   it('saves the username and email to database', (done) => {
-    postValidUser().then(() => {
+    postUser().then(() => {
       // query user table
       User.findAll().then((userList) => {
         const savedUser = userList[0];
@@ -56,7 +58,7 @@ describe('User Registration', () => {
   });
 
   it('hashes the password in database', (done) => {
-    postValidUser().then(() => {
+    postUser().then(() => {
       // query user table
       User.findAll().then((userList) => {
         const savedUser = userList[0];
@@ -64,5 +66,37 @@ describe('User Registration', () => {
         done();
       });
     });
+  });
+
+  it('returns 400 when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('returns validationErrors field in response body when validation errors occurs', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+
+    const body = response.body;
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('returns username cannot be null when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+
+    const body = response.body;
+    expect(body.validationErrors.username).toBe('Username cannot be null');
   });
 });
